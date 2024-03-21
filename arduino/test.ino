d4d94ad605c2c81d85b316408297ac82935c5a8b
Alzeuwu <yt689424@gmail.com>
2024-03-21 18:41:38 +0800

    新增arduino程式

---
+++ arduino/test.ino
@@ -0,0 +1,21 @@
+const int analogPin = A0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(analogPin);
+  float voltage = sensorValue * (5.0 / 1023.0);
+  float acceleration = (voltage - 1.61) * 10;  // Adjust this factor based on your observation
+
+ // Serial.print("Sensor Value: ");
+ // Serial.print(sensorValue);
+ // Serial.print("\tVoltage: ");
+ // Serial.print(voltage);
+  //Serial.print("V\tAcceleration: ");
+  Serial.print(acceleration);
+  Serial.println(" g");
+
+  delay(10);
+}
