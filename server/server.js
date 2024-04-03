19fd62db2584b590a966c7805c2a4f67b0717b25
lyuchan <david12312330@gmail.com>
2024-04-03 14:07:53 +0800

    u.3

---
+++ server/server.js
@@ -0,0 +1,56 @@
+// 引入必要的模組
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+// 創建 Express 應用程式
+const app = express();
+
+// 設定路由處理程序
+app.get('/api', (req, res) => {
+    const { token, freq, up } = req.query; // 從查詢參數中獲取 freq 和 up 的值
+    const timestamp = new Date().toISOString(); // 獲取當前時間
+    try {
+        fs.accessSync(`./api/${token}.json`, fs.constants.F_OK);
+        console.log('檔案已存在。');
+    } catch (err) {
+        // 檔案不存在，可以在這裡新增一個檔案
+        try {
+            fs.writeFileSync(`./api/${token}.json`, '[]');
+            console.log('成功新增檔案！');
+        } catch (writeErr) {
+            console.error('無法新增檔案：', writeErr);
+        }
+    }
+
+    // 讀取現有的資料
+    const filePath = path.join(__dirname, 'api', `${token}.json`);
+    let existingData = [];
+    try {
+        const dataBuffer = fs.readFileSync(filePath);
+        existingData = JSON.parse(dataBuffer.toString());
+        console.log(existingData)
+    } catch (error) {
+        console.error('Error reading existing data:', error.message);
+    }
+
+    // 將新資料追加到 data.json 檔案
+    const newData = { freq, up, timestamp };
+    try {
+        existingData.push(newData);
+    } catch (error) {
+        existingData = [existingData];
+        existingData.push(newData);
+    }
+
+    fs.writeFileSync(filePath, JSON.stringify(existingData, null, 2));
+
+    res.send('Data saved successfully!');
+
+});
+
+// 啟動伺服器
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
