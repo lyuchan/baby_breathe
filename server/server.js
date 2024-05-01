041a02244a76cb2ccdfed746a623cae602d63249
lyuchan <david12312330@gmail.com>
2024-05-01 13:30:32 +0800

    整合linebot

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
 // 引入必要的模組
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
+require('dotenv').config();//環境變數
+const fs = require('fs');//系統文件
+const path = require('path');//目錄
+const express = require('express');//網頁
+const { Client, middleware } = require('@line/bot-sdk');//line bot
 
 // 創建 Express 應用程式
 const app = express();
 app.use(express.static(__dirname + "/web"));
 // 設定路由處理程序
+
+//api功能
 app.get('/api', (req, res) => {
     const { token, freq, up } = req.query; // 從查詢參數中獲取 freq 和 up 的值
     //如果token或freq或up是undefine則返回data error!
@@ -53,6 +57,92 @@ app.get('/api', (req, res) => {
     res.send('Data saved successfully!');
 
 });
+
+
+//linebot功能
+const lineConfig = {
+    channelAccessToken: process.env["CHANNEL_ACCESS_TOKEN"],
+    channelSecret: process.env["CHANNEL_SECRET"]
+};
+const client = new Client(lineConfig);
+app.post('/linebotwebhook', middleware(lineConfig), async (req, res) => {
+    try {
+        /*{  req.body長這樣
+            "events": [
+                {
+                    "type": "message",
+                    "replyToken": "uuidhere",
+                    "source": {
+                        "userId": "xxxx"",
+                        "type": "user"
+                    },
+                    "timestamp": 1592813446208,
+                    "mode": "active",
+                    "message": {
+                        "type": "text",
+                        "id": "12188948022060",
+                        "text": "X"
+                    }
+                }
+            ],
+            "destination": "xxx"
+        }*/
+        let result = await req.body.events.map(handleEvent);
+        res.json(result);
+    }
+    catch (err) {
+        console.log(err);
+    }
+});
+
+const handleEvent = (event) => {
+    switch (event.type) {
+        case 'join': //這隻機器人加入別人的群組
+            break;
+        case 'follow': //追蹤這隻機器人
+            break;
+        case 'message': //傳訊息給機器人
+            switch (event.message.type) {
+                case 'text':
+                    textHandler(event.replyToken, event.message.text);   //測試code就不用這行
+                    //             return client.replyMessage(replyToken, {     ---->    測試用code通常就是呼叫client.replyMessage，並依api要求格式回傳
+                    //                 type: 'text',
+                    //                 text: event.message.text  ----> 我們傳給機器人的文字會在這裡面
+                    //             });
+                    break;
+                case 'sticker':
+                    // do sth with sticker
+                    return
+            }
+    }
+}
+
+const textHandler = (replyToken, inputText) => {
+    try {
+        let resText;
+        switch (inputText) {
+            case '你好':
+                resText = '你好啊';
+                break;
+            case 'test':
+                resText = `測試`;
+                break
+            //             case 'Q&A':
+            //                 return client.replyMessage(replyToken, imageMap());
+            //             case 'q&a':
+            //                 return client.replyMessage(replyToken, carousel());
+            default:
+                resText = '請親臨院所';
+        }
+        return client.replyMessage(replyToken, {
+            type: 'text',
+            text: resText
+        });
+    } catch (err) {
+        console.log(err)
+    }
+
+}
 
 // 啟動伺服器
 const port = process.env.PORT || 3000;
