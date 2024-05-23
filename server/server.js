d41d30cb33f8454e4c92c36840fe91df4939a084
lyuchan <david12312330@gmail.com>
2024-05-23 23:09:25 +0800

    debug

--- server/server.js
+++ server/server.js
@@ -158,6 +158,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-/*lineapp.listen(port + 1, () => {
+lineapp.listen(port + 1, () => {
     console.log(`Line server is running on port ${port + 1}`);
-});*/
+});
