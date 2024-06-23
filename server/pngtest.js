7a7ca15079e668984a2b8afa5fa033a137f1e19b
BuildTools <unconfigured@null.spigotmc.org>
2024-06-23 19:18:11 +0800

    debug

--- server/pngtest.js
+++ server/pngtest.js
@@ -11,7 +11,7 @@ async function test ()  {
     // 设置浏览器视窗
     page.setViewport({
         width: 1920,
-        height: 1080,
+        height: 3200,
     })
 
     await page.goto('https://medium.com/appxtech/puppeteer-%E9%81%8B%E7%94%A8-%E4%B8%80-%E7%B6%B2%E9%A0%81%E6%88%AA%E5%9C%96%E8%88%87pdf%E8%BC%B8%E5%87%BA-d9589588a3a8');
