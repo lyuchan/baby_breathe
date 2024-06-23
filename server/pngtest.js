d27a655f957c11c2fb6fdc1aa899f3c068a5f0bf
BuildTools <unconfigured@null.spigotmc.org>
2024-06-23 19:33:03 +0800

    debug

--- server/pngtest.js
+++ server/pngtest.js
@@ -15,7 +15,7 @@ async function test() {
         height: 1080,
     })
 
-    await page.goto('https://medium.com/appxtech/puppeteer-%E9%81%8B%E7%94%A8-%E4%B8%80-%E7%B6%B2%E9%A0%81%E6%88%AA%E5%9C%96%E8%88%87pdf%E8%BC%B8%E5%87%BA-d9589588a3a8');
+    await page.goto('https://db.lyuchan.com/pic');
     
     await page.screenshot({ path: './web/img/baidu.png', fullPage: false });
     await browser.close();
