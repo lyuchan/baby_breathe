51d5fc137dc4e1fb8cd77b15d5e3bd7d567e4efd
BuildTools <unconfigured@null.spigotmc.org>
2024-06-23 19:26:47 +0800

    debug

--- server/pngtest.js
+++ server/pngtest.js
@@ -12,11 +12,11 @@ async function test() {
     // 设置浏览器视窗
     page.setViewport({
         width: 1920,
-        height: 3200,
+        height: 1080,
     })
 
     await page.goto('https://medium.com/appxtech/puppeteer-%E9%81%8B%E7%94%A8-%E4%B8%80-%E7%B6%B2%E9%A0%81%E6%88%AA%E5%9C%96%E8%88%87pdf%E8%BC%B8%E5%87%BA-d9589588a3a8');
-    await page.waitForFunction(() => window.puppeteerReadyState === 'complete');
+    
     await page.screenshot({ path: './web/img/baidu.png', fullPage: false });
     await browser.close();
 
